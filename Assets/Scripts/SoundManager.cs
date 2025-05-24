8178a9da1a8f30b6e02f4ee308fd94cc1d9f50b8
FateyevStanislav <fateev0803@gmail.com>
2025-05-24 18:58:08 +0500

    музыка на 1-3 уровень

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
